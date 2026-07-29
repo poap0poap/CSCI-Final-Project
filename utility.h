db3488239e37d7553cf09e3e53808abab51430a3
poap0poap <30390818+poap0poap@users.noreply.github.com>
2026-07-29 15:50:18 -0600

    Create utility.h

---
+++ utility.h
@@ -0,0 +1,11 @@
+//some usefull visual funky stuff. make for less blicky transitions when stuff doesnt need to be cleaned
+
+#ifndef UTILITY_H
+#define UTILITY_H
+
+void clearBelowLine(int line);
+void clearScreen();
+void moveCursorToTop();
+
+
+#endif
